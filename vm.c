c79b06feecedbcccdff034f8530bbcaf17853c5f
mikaylaphilpot <75904361+mikaylaphilpot@users.noreply.github.com>
2025-09-05 11:35:57 -0400

    create file

---
+++ vm.c
@@ -0,0 +1,22 @@
+/*
+Assignment :
+vm . c - Implement a P - machine virtual machine
+Authors : Mikayla Philpot and Sindhu Sesham
+Language : C ( only )
+To Compile :
+gcc - O2 - Wall - std = c11 -o vm vm . c
+To Execute ( on Eustis ) :
+./ vm input . txt
+where :
+input . txt is the name of the file containing PM /0 instructions ;
+each line has three integers ( OP L M )
+Notes :
+- Implements the PM /0 virtual machine described in the homework
+instructions .
+- No dynamic memory allocation or pointer arithmetic .
+- Does not implement any VM instruction using a separate function .
+- Runs on Eustis .
+Class : COP 3402 - Systems Software - Fall 2025
+Instructor : Dr . Jie Lin
+Due Date : Friday , September 12 th , 2025
+*/
